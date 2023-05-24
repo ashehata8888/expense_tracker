e4062444b65de007c7a7d7f10bab26cb9418563b
ashehata8888 <47960845+ashehata8888@users.noreply.github.com>
2023-05-24 16:24:10 +0300

    Thanks to Allah functionality Done

--- src/App.js
+++ src/App.js
@@ -6,39 +6,6 @@ import DynamicTable from "./DynamicTable";
 
 function App() {
 
-  const [budget,setBudget] = useState("")
-
-  const d = new Date();
-let month = d.getMonth() + 1 ;
-
-const currMonth = (m) =>{
-  if(m === 1){
-    return "Jan"
-  } else if (m === 2){
-    return "Feb"
-  } else if (m === 3){
-    return "Mar"
-  } else if (m === 4){
-    return "Apr"
-  } else if (m === 5){
-    return "May"
-  } else if (m === 6){
-    return "Jun"
-  } else if (m === 7){
-    return "Jul"
-  } else if (m === 8){
-    return "Aug"
-  } else if (m === 9){
-    return "Sep"
-  } else if (m === 10){
-    return "Oct"
-  } else if (m === 11){
-    return "Nov"
-  } else if (m === 12){
-    return "Dec"
-  }
-   
-} 
 
 
 
@@ -52,17 +19,7 @@ const currMonth = (m) =>{
        Expenses Tracker
       </header>
         
-        <div className="budget">
-         <span>{currMonth(month)}</span>  Budget <span>
-          <input
-          className="budgetTxt"
-          placeholder='Monthly budget'
-         type='text'
-         onChange={(e)=>setBudget(e.target.value)}
-         value={budget}
-         ></input>
-         </span>
-        </div>
+        
 
         <DynamicTable />
 
--- src/DynamicTable.jsx
+++ src/DynamicTable.jsx
@@ -2,32 +2,124 @@ import { useEffect, useState } from "react";
 function DynamicTable(){
 
   const [TableData,setTableData] = useState([{
-    Id: "" , Description:"", Amount:"" ,Action:""
+    Id: "" , Description:"", Amount:0 ,Action:""
   }])
   const [description,setDescription] = useState('')
   const [amount, setAmount] = useState(0)
+  const [realDelete,setRealDelete] = useState(0)
+  const [dayId,setDayId] = useState(0)
+  const [totalExp , setTotalExp] = useState(0)
+  const [budgetRem,setBudgetRem] = useState(0)
+
+  const [budget,setBudget] = useState("")
+
+  const d = new Date();
+let month = d.getMonth() + 1 ;
+
+const currMonth = (m) =>{
+  if(m === 1){
+    return "Jan"
+  } else if (m === 2){
+    return "Feb"
+  } else if (m === 3){
+    return "Mar"
+  } else if (m === 4){
+    return "Apr"
+  } else if (m === 5){
+    return "May"
+  } else if (m === 6){
+    return "Jun"
+  } else if (m === 7){
+    return "Jul"
+  } else if (m === 8){
+    return "Aug"
+  } else if (m === 9){
+    return "Sep"
+  } else if (m === 10){
+    return "Oct"
+  } else if (m === 11){
+    return "Nov"
+  } else if (m === 12){
+    return "Dec"
+  }
+   
+} 
+
 
 
   console.log("TableData is ...",TableData)
 
-  let dayId = TableData != undefined && TableData.length - 1
+  // let dayId = TableData != undefined && TableData.length - 1
 
-  const realDelete = (e,inx) =>{
-    console.log("test for inx of Delete Obj...",e)
+// useEffect(()=>{
 
-    let clonforDel = [...TableData]
-    clonforDel.filter((item)=> inx !== item.Id  )
-    // clonforDel.splice(inx,0)
-    setTableData(clonforDel)
+//   const sumObj = TableData.reduce(
+//     (a, o) => (
+//       Object.entries(o).forEach(
+//         ([k, v]) => (
+
+//           (a["Amount"] += v)
+//           // (a[k] = { ...(a[k] ?? { sum: 0, total: 0 }) }),
+//           // (a[k]['sum'] += v),
+//           // (a[k]['total'] += 1)
+//         )
+//       ),
+//       a
+//     ),
+//   );
+  
+//   console.log("sumObj",sumObj);
+
+// })
+
+
+
+
+
+let clonforDel = [...TableData]
+useEffect(()=>{
+  console.log("clonforDel",clonforDel)
+
+  const newList = clonforDel.filter((item) => item.Id !== realDelete);
+  setTableData(newList)
+let totalArr = []
+  let total = 0
+  for(let x = 0 ; x < newList.length ; x++){
+     total = total + TableData != undefined && newList[x]["Amount"]
+     console.log("totalTest", TableData != undefined && newList[x]["Amount"])
+     console.log("testX",x)
+    //  setTotalExp(total)
+    totalArr.push(parseInt(newList[x]["Amount"]))
 
   }
+  // console.log("totalArrTest",totalArr)
+  const sum = totalArr.reduce((partialSum, a) => partialSum + a, 0);
+console.log("totalArrTest",sum); // 6
+
+  setTotalExp(sum)
+
+  setBudgetRem(budget - sum)
+},[realDelete])
+
+
+
 
   const trashIconButton = (inx)=>{
 
+    // console.log("testCloneDelIn",clonforDel)
+
+    const onclickTrash = (inx) =>{
+
+      setRealDelete(inx)
+      // console.log("testCloneDelIn",clonforDel)
+    
+    
+    }
 
     console.log("test for inx of Delete Obj...",inx)
     return(
-      <button onClick={(e)=>realDelete(e,inx)}>🗑️</button>
+      <button onClick={(e)=>{onclickTrash(inx)}
+    }>🗑️</button>
     )
 
   }
@@ -42,6 +134,9 @@ function DynamicTable(){
 
   const addExp = ()=>{
     // e.preventDefault()
+    // dayId = TableData != undefined && dayId + 1
+
+    setDayId(TableData != undefined && dayId + 1)
 
     let mainObj = {Id: dayId , Description:`${description}`, Amount:amount ,Action:trashIconButton(dayId)}
     let cloneTableData = TableData != undefined && [...TableData]
@@ -53,6 +148,36 @@ function DynamicTable(){
 
     // setTimeout(()=>console.log(TableData),0)
     console.log(TableData)
+
+    // const newList = clonforDel.filter((item) => item.Id !== realDelete);
+    // setTableData(newList)
+  let totalArr = []
+    let total = 0
+    setTimeout(()=>{
+
+      for(let x = 0 ; x < cloneTableData.length ; x++){
+        total = total + cloneTableData != undefined && cloneTableData[x]["Amount"]
+        console.log("totalTest", cloneTableData[x]["Amount"])
+        console.log("testX",x)
+       //  setTotalExp(total)
+       totalArr.push(parseInt(cloneTableData[x]["Amount"]))
+      //  totalArr.push(parseInt(amount))
+      console.log("totalArr..",totalArr)
+       
+   //     const sum = totalArr.reduce((partialSum, a) => partialSum + a, 0);
+   // console.log("totalArrTest",sum); // 6
+     }
+     // console.log("totalArrTest",totalArr)
+     const sum = totalArr.reduce((partialSum, a) => partialSum + a, 0);
+   console.log("totalArrTest",sum); // 6
+   
+    setTotalExp(sum)
+
+    setBudgetRem(budget - sum)
+    },0)
+   
+
+
   }
 
 //   const TableData=[
@@ -100,6 +225,18 @@ const tdData =() =>{
   return (
 <>
 
+<div className="budget">
+         <span>{currMonth(month)}</span>  Budget <span>
+          <input
+          className="budgetTxt"
+          placeholder='Monthly budget'
+         type='text'
+         onChange={(e)=>setBudget(e.target.value)}
+         value={budget}
+         ></input>
+         </span>
+        </div>
+
     <div className="budget">
         Description <span>
           <input
@@ -120,6 +257,11 @@ const tdData =() =>{
          ></input>
          </span>
          <button onClick={()=>addExp()}>Add Expense</button>
+        </div>
+
+        <div>
+          <span className="budgetTxt"> Total Expenses : </span> <span className="budgetTxt"> {totalExp}</span>
+          <span className="budgetTxt"> Remain Budget :  </span> <span className="budgetTxt">{budgetRem}</span>
         </div>
 
 
